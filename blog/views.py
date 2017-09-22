04759efc4b8d02c65f247d9c368b920192bc9a79
Daniele Zanotelli <dazano@gmail.com>
2017-09-22 20:33:14 +0200

    implemented details page

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def post_list(request):
         published_date__lte=timezone.now()
     ).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
